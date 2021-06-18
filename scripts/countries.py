7b8e9b8cf5f687e90999aba2de742e92bf7c6a06
Steve Roe <steveroe@gmail.com>
2021-06-18 07:58:16 -0700

    Add more countries, don't report dailies.

--- scripts/countries.py
+++ scripts/countries.py
@@ -2,14 +2,24 @@
 
 alternate_country_names_to_country = {
     'Bahamas, The': 'Bahamas',
+    'Congo (Brazzaville)': 'Republic of the Congo',
+    'Czech Republic': 'Czechia',
     'Gambia, The': 'Gambia',
+    'Holy See': 'Vatican City',
     'Hong Kong SAR': 'Hong Kong',
     'Iran (Islamic Republic of)': 'Iran',
-    'Macao SAR': 'Macau',
+    'Ivory Coast': "Cote d'Ivoire",
+    'Korea, South': 'South Korea',
+    'Macao': 'China',
+    'Macao SAR': 'China',
     'Mainland China': 'China',
+    'Republic of Ireland': 'Ireland',
+    'Republic of Korea': 'South Korea',
+    'Taipei and environs': 'Taiwan',
     'Taiwan*': 'Taiwan',
     'The Bahamas': 'Bahamas',
     'The Gambia': 'Gambia',
+    'UK': 'United Kingdom',
     'Viet Nam': 'Vietnam',
 }
 
